90d104b5c1f9fc83cad43a15b15bbddb942b7007
matrim <matrim@ec367e15-c9dc-451f-af9c-cc3cdbb20dec>
2010-05-31 20:24:39 +0000

    Updating to v0.7.9

--- trunk/webtexter/src/main.h
+++ trunk/webtexter/src/main.h
@@ -47,7 +47,7 @@ G_BEGIN_DECLS
  * Please do not modify "APP_NAME" (or other defines) to different name
  */
 #define APP_NAME "webtexter"
-#define APP_VER "0.7.8"
+#define APP_VER "0.7.9"
 #define APP_SERVICE "org.maemo.webtexter"
 #define APP_METHOD "/org/maemo/webtexter"
 /* end defines */
